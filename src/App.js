2bfa0d43826d4bda163a7c187297ee9d1c2a25b1
debora soares e silva monteiro <gscmonteiro@gmail.com>
2021-07-21 23:25:20 -0300

    Formulario para cadastro de usuario concluido, adiciona ao local storage. WIP front end

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ import Input from './components/Form/input';
 
 function App() {
   function handleSubmit(data) {
-    console.log(data);
+    // localStorage.setItem('fullName', data.fullName);
+    // localStorage.setItem('email', data.email);
+    // localStorage.setItem('cpf', data.cpf);
+    // localStorage.setItem('address.street', data.address.street);
+    // localStorage.setItem('address.number', data.address.number);
+    // localStorage.setItem('address.district', data.address.district);
+    // localStorage.setItem('address.city', data.address.city);
+    // localStorage.setItem('address.state', data.address.state);
+    
+    let user = {
+      name: data.fullName,
+      email: data.email,
+      cpf: data.cpf,
+      address: {
+        street: data.address.street,
+        number: data.address.number,
+        district: data.address.district,
+        city: data.address.city,
+        state: data.address.state
+      }
+    }
+    console.log(user);
+    localStorage.setItem('user', JSON.stringify(user));
   }
   return (
     <div className="App">
